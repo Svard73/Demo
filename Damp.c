7f1280541a156cb1c8cacdc5e566c61a47b01995
Svard73 <s.v.a.r.d.cs@gmail.com>
2017-01-11 14:34:15 +0100

    first commit

--- Damp.c
+++ Damp.c
@@ -3,6 +3,15 @@
 
 int main()
 {
+	 printf("Please Enter a Number between 1 and 10 (1-10): ");
+ scanf("%d", &value);
+
+while ( value < 1 || value > 10)
+  {
+
+    printf("Incorrect input, please try again.\n");
+    printf("Enter a Number between 1 and 10 (1-10): ");
+    scanf("%d", &value);
     printf("Hello world!\n");
     return 0;
 }
